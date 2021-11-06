cd45973cda7d4f972c7a23a5a07a52304931a882
Brian Duong <brianminhduong@gmail.com>
2021-11-06 15:24:01 +0000

    Created Account class

--- index.js
+++ index.js
@@ -28,9 +28,20 @@ class Deposit {
 
 }
 
+//Class to create a new account
+class Account {
+
+  //Default values
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
 t1 = new Withdrawal(50.25);
 t1.commit();
