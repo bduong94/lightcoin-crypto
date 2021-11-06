787944efc88de3e62ee1ce68374e5a88c4fb7331
Brian Duong <brianminhduong@gmail.com>
2021-11-06 22:14:49 +0000

    Added method to retrieve balance at any point, transaction history and not allow for any withdrawals that exceed balance

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ class Account {
   //Default values
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactionHistory = [];
+  }
+
+  get balance() {
+    let balance = 0;
+
+    for (let i = 0; i < this.transactionHistory.length; i++) {
+      balance += this.transactionHistory[i].amount;
+    }
+
+    return balance;
+  }
+
+  //Adds each transaction object into the array
+  addTransacation(transaction) {
+    this.transactionHistory.push(transaction);
   }
 
 }
@@ -19,22 +34,48 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+
+  commit() {
+    if (this.isAllowed) {
+      this.amount = this.value;
+      this.time = new Date();
+      this.account.addTransacation(this);
+    } else (
+
+      console.log(`Cannot complete transaction due to ${this.account.balance} is less than ${this.amount}.`)
+
+    );
+  }
+
 }
 
 class Withdrawal extends Transaction {
 
-  //Subtracts from current balance
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
+  }
+
+  get isAllowed() {
+
+    if ((this.account.balance - this.amount) > 0) {
+
+      return true;
+
+    }
+
+    return false;
   }
 
 }
 
 class Deposit extends Transaction {
 
-  //Adds into current balance
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
+  }
+
+  get isAllowed() {
+    return true;
   }
 
 }
@@ -44,8 +85,30 @@ class Deposit extends Transaction {
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25, myAccount);
+let t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-// console.log('Balance:', balance);
+let t2 = new Deposit(100, myAccount);
+t2.commit();
+
+let t3 = new Withdrawal(150, myAccount);
+t3.commit();
+
+console.log(`Account balance for ${myAccount.username}: $${myAccount.balance}`);
+
+const mySavingsAccount = new Account("FriendlyPie");
+
+let st1 = new Deposit(50.25, mySavingsAccount);
+st1.commit();
+
+let st2 = new Deposit(100, mySavingsAccount);
+st2.commit();
+
+let st3 = new Deposit(200, mySavingsAccount);
+st3.commit();
+
+let st4 = new Withdrawal(300, mySavingsAccount);
+st4.commit();
+
+
+console.log(`Account balance for ${mySavingsAccount.username}: $${mySavingsAccount.balance}`);
