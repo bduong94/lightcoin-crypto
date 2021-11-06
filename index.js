6e8d223b9217153241dd2de5a170747920317094
Brian Duong <brianminhduong@gmail.com>
2021-11-06 15:32:00 +0000

    Updated Withdrawal and Deposit classes to utilize instance properties from the Account class

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ let balance = 500.00;
 class Withdrawal {
 
   //Default values
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   //Subtracts from current balance
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 
 }
@@ -17,13 +18,14 @@ class Withdrawal {
 class Deposit {
 
   //Default values
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   //Adds into current balance
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 
 }
@@ -43,16 +45,8 @@ class Account {
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-t3 = new Deposit(120.00);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-console.log('Balance:', balance);
+// console.log('Balance:', balance);
